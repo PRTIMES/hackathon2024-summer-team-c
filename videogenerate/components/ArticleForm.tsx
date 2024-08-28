840846b4df7881b9c2e7a616b82ecd11945b2150
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-08-28 17:56:59 +0900

    サイトタイトル追加、記事フォームwo

--- videogenerate/components/ArticleForm.tsx
+++ videogenerate/components/ArticleForm.tsx
@@ -36,7 +36,12 @@ export default function ArticleForm() {
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           placeholder="Leave a comment..."
         ></input>
-        <button className="bg-blue-700 text-">生成</button>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          生成
+        </button>
       </form>
     </div>
   );
---
+++ videogenerate/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="text-white bg-[#2a4b7a]">サイトタイトル</header>;
+}
