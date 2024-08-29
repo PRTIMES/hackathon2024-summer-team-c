35df2ad61c4ff4688ffe68a920151e25f32b1f5f
t38miwa <t38miwa@gmail.com>
2024-08-29 17:54:38 +0900

    デザイン、css修正-2

--- videogenerate/components/Snslink.tsx
+++ videogenerate/components/Snslink.tsx
@@ -2,14 +2,14 @@ import { FaYoutube, FaTiktok, FaInstagram } from 'react-icons/fa';
 
 export default function Snslink() {
     return (
-        <div className="video-container">
-            <a className="flex items-center text-red-600 text-lg font-medium hover:underline" href="https://www.youtube.com/?gl=JP&hl=ja&app=desktop">
+        <div className="flex flex-col items-center justify-center min-auto text-center p-8">
+            <a className="flex items-center text-black text-lg font-medium hover:underline mb-2" href="https://www.youtube.com/?gl=JP&hl=ja&app=desktop">
                 <FaYoutube className="mr-2 text-red-600" /> Youtube Short
             </a>
-            <a className="flex items-center text-black text-lg font-medium hover:underline" href="https://www.tiktok.com/ja-JP/">
+            <a className="flex items-center text-black text-lg font-medium hover:underline mb-2" href="https://www.tiktok.com/ja-JP/">
                 <FaTiktok className="mr-2 text-black" /> TikTok
             </a>
-            <a className="flex items-center text-pink-500 text-lg font-medium hover:underline" href="https://www.instagram.com/">
+            <a className="flex items-center text-black text-lg font-medium hover:underline" href="https://www.instagram.com/">
                 <FaInstagram className="mr-2 text-pink-500" /> Instagram Reels
             </a>
         </div>
