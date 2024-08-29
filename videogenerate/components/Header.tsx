4921429f8f25af7ef226aee244d17a161ee683c5
t38miwa <t38miwa@gmail.com>
2024-08-29 09:54:23 +0900

    Result,Pv新規作成、ArticleForm,Video,Link修正

--- videogenerate/components/Header.tsx
+++ videogenerate/components/Header.tsx
@@ -1,3 +1,3 @@
 export default function Header() {
-  return <header className="text-white bg-[#2a4b7a]">サイトタイトル</header>;
+  return <header className="text-white bg-[#2a4b7a]">PR_TIMES AI動画生成</header>;
 }
--- videogenerate/components/Link.tsx
+++ videogenerate/components/Link.tsx
@@ -1,14 +1,16 @@
+import { FaYoutube, FaTiktok, FaInstagram } from 'react-icons/fa';
+
 export default function Link() {
     return (
         <div className="video-container flex flex-col items-center justify-center min-h-screen bg-gray-50 space-y-4">
-            <a className="text-blue-500 text-lg font-medium hover:underline" href="">
-                Youtube Short
+            <a className="flex items-center text-blue-500 text-lg font-medium hover:underline" href="https://www.youtube.com/?gl=JP&hl=ja&app=desktop">
+                <FaYoutube className="mr-2" /> Youtube Short
             </a>
-            <a className="text-blue-500 text-lg font-medium hover:underline" href="">
-                TikTok
+            <a className="flex items-center text-blue-500 text-lg font-medium hover:underline" href="https://www.tiktok.com/ja-JP/">
+                <FaTiktok className="mr-2" /> TikTok
             </a>
-            <a className="text-blue-500 text-lg font-medium hover:underline" href="">
-                Instagram Reels
+            <a className="flex items-center text-blue-500 text-lg font-medium hover:underline" href="https://www.instagram.com/">
+                <FaInstagram className="mr-2" /> Instagram Reels
             </a>
         </div>
     )
