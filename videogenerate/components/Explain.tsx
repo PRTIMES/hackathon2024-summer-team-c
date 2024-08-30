ecdd3297fb27542b486bf2dd0f38f2d1481518c8
t38miwa <t38miwa@gmail.com>
2024-08-30 10:41:31 +0900

    explain追加

---
+++ videogenerate/components/Explain.tsx
@@ -0,0 +1,9 @@
+export default function Explain() {
+    return (
+        <div className="result-container p-6 rounded-xl space-y-4 m-4 flex-1 max-w-[calc(50%-2rem)] flex items-center h-full mx-auto max-w-6xl">
+        <p className="leading-[3rem] tracking-[0.1rem] text-justify">
+          このサイトではすでにリリースした記事をもとに一瞬でショート動画を作成することができます。またその内容の修正や字幕の編集、サムネイルの設置など動画を作成するにあたって必要なコンテンツはすべて入力することができ、それをもとに動画を作成できます。
+        </p>
+      </div>
+    )
+}
